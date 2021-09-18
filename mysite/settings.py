c47e1024fafe600b96715f6f0976681b1b734922
SoravitPete <esterpiano.official@gmail.com>
2021-09-18 23:15:18 +0700

    make .env file then remove the value SECRET_KEY

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
+import environ
 
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,10 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yn4xyrpbj6cyycz)+j70_%b)p4kyb2@iijzu_i5up#3vm0_x)@'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG',default = False)
 
 ALLOWED_HOSTS = []
 
